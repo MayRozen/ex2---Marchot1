f7b26fc45368a1663a487956e660c106c7f58157
May Rozen <154542312+MayRozen@users.noreply.github.com>
2024-02-11 00:49:41 +0200

    Update my_graph.c

--- my_graph.c
+++ my_graph.c
@@ -4,7 +4,7 @@
 
 int main(){
     char input = 'z';
-    int graph[10][10];
+    int graph[GRAPH_SIZE][GRAPH_SIZE];
 
     printf("Please enter A, B or C to start or D or EOF if tou want to get out:");
     
