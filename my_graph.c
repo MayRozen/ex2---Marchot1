0e3c355e926726d9468c479c8d1ea985c6119df1
May Rozen <154542312+MayRozen@users.noreply.github.com>
2024-02-12 18:41:15 +0200

    Update my_graph.c

--- my_graph.c
+++ my_graph.c
@@ -13,24 +13,24 @@ int main(){
         case 'A':
             putInValues(graph);
             floydWarshall(graph);
-            
             break;
+
         case 'B':
             ifPathItoJ(graph);
-
             break;
+
         case 'C':
             shortestPath(graph);
-
             break;
-        case 'D':
 
+        case 'D':
             return 0;
             break;
-        case EOF:
 
+        case EOF:
             return 0;     
             break;
+            
         default:
             break;
         } 
